bc4f42d602c609200d5e3d5a03ca61ac7e2e67a5
EriickChavez <10erickchavez10@gmail.com>
2023-12-17 18:37:19 -0600

    upload album

--- src/Infrastructure/Store/Actions/ProductAction.ts
+++ src/Infrastructure/Store/Actions/ProductAction.ts
@@ -3,6 +3,7 @@ import {Product, ProductInput} from '../../../Domain/Entity';
 import RNFetchBlob from 'rn-fetch-blob';
 import {Config} from '../../../Config/ENV';
 import {buildFetchBodyProduct} from '../../../Utils/productBodyConstructor';
+import {Platform} from 'react-native';
 
 export const fetchNewProducts = createAsyncThunk<
   {
@@ -44,5 +45,70 @@ export const fetchNewProducts = createAsyncThunk<
     } catch (err) {
       throw new Error(`${err}`);
     }
+  },
+);
+
+export const fetchPictureAlbum = createAsyncThunk<
+  {
+    data: Product;
+    error: string | null;
+    status: number;
+  },
+  {newProduct: ProductInput; type: string}
+>(
+  '',
+  async ({
+    newProduct,
+  }): Promise<{
+    data: Product;
+    error: string | null;
+    status: number;
+  }> => {
+    const URL: string = Config.API_URL + '/product/add/album';
+    const METHOD = 'POST';
+    const HEADERS = {'Content-Type': 'multipart/form-data'};
+    const userId = 'soyUnIdDePrueba'; // newProduct.userId;
+    try {
+      let index = 0;
+      const filename =
+        userId + '*' + index + '.album-' + newProduct.title + '.jpg';
+      const album = newProduct?.album ?? [];
+      if (album.length > 0) {
+        RNFetchBlob.fetch(METHOD, URL, HEADERS, [
+          {
+            name: 'album',
+            filename,
+            type: 'image/jpg',
+            data: RNFetchBlob.wrap(
+              Platform.OS === 'ios'
+                ? album?.[0].replace('file://', '')
+                : album?.[0],
+            ),
+          },
+        ])
+          .then(resp => {
+            const resInJson = JSON.parse(resp.data);
+            console.info({resInJson});
+            return {
+              data: resInJson,
+              error: null,
+              status: 200,
+            };
+          })
+          .catch(err => {
+            console.error(err);
+          });
+        index++;
+      } else {
+        console.log('no hay album');
+      }
+    } catch (err) {
+      console.warn({err});
+    }
+    return {
+      data: newProduct as Product,
+      error: null,
+      status: 200,
+    };
   },
 );
